7c272aa602e7f27f97485b486bd011636b61ae71
Paul Underwood <paul.und@gmail.com>
2016-10-17 18:33:58 +0100

    Add router history mode

---
+++ router-history/js/app.js
@@ -0,0 +1,20 @@
+const Home = { template: '<div class="container"><h1>Home</h1></div>' };
+const About = { template: '<div class="container"><h1>About</h1></div>' };
+const Contact = { template: '<div class="container"><h1>Contact</h1></div>' };
+
+const routes = [
+    {
+        path: '/',
+        component: Home
+    },
+    {
+        path: '/about',
+        component: About
+    },
+    {
+        path: '/contact',
+        component: Contact
+    }
+];
+const router = new VueRouter({ routes: routes, mode: 'history' });
+const app = new Vue({ router }).$mount('#app');

8a18143a84b3fc7fee3bb2c45aa195ffe6f64652
Paul Underwood <paul.und@gmail.com>
2016-10-19 17:59:06 +0100

    Added a single file todo component.

---
+++ hello-world/single-file-todo-component.vue
@@ -0,0 +1,17 @@
+<template>
+    <li>{{ todo.task }}</li>
+</template>
+
+<style>
+    li
+    {
+        font-size: 125%;;
+    }
+</style>
+
+<script>
+    export default
+    {
+        props: ['todo'],
+    }
+</script>
